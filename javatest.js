acbe4fa18f09d438b0911dc66d54df10b3655b28
cmarianielsen@gmail.com <celi2004@elev.tec.dk>
2025-03-11 16:31:45 +0100

    Tilføjer burgermenu, prøver at fikse favicon

--- javatest.js
+++ javatest.js
@@ -1,8 +1,9 @@
-/* function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  } */
+
+    const hamMenu = document.querySelector('.ham-menu');
+
+    const offScreenMenu = document.querySelector('.off-screen-menu');
+
+  hamMenu.addEventListener('click', () => { 
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
